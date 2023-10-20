d2ca9e107956d2e98ce85e640d386478028f0138
Eray Keskin <meraykeskin@gmail.com>
2023-10-20 22:25:11 +0300

    add next-theme theme-switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "@/components/providers/theme-providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ThemeProvider
+        <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
@@ -29,8 +29,7 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
-        
-        </body>
+          </body>
       </html>
     </ClerkProvider>
   );
