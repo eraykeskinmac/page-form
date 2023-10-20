177a748bdf40f82cc2ce4c9f554164ef1541c08b
Eray Keskin <meraykeskin@gmail.com>
2023-10-20 22:30:12 +0300

    add navbar layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,10 +5,12 @@ import { ReactNode } from "react";
 
 function Layout({ children }: { children: ReactNode }) {
   return <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-    <nav className="flex justi">
+    <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
       <Logo/>
+      <div className="flex gap-4 items-center">
       <ThemeSwitcher/>
       <UserButton afterSignOutUrl="/sign-in"/>
+      </div>
     </nav>
     <main className="flex w-full flex-grow">{children}</main>
   </div>
