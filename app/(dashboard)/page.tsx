6792d70135afc5d701bcf55c5c67437ffcdb5025
Eray Keskin <meraykeskin@gmail.com>
2023-11-03 15:43:19 +0300

    Add form card viewer action button and badge and redirect to builder page after creating the form

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,14 +1,26 @@
 import { GetFormStats, GetForms } from "@/actions/form";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { ReactNode, Suspense } from "react";
 import { LuView } from "react-icons/lu";
-import { FaWpforms } from "react-icons/fa";
+import { FaEdit, FaWpforms } from "react-icons/fa";
 import { HiCursorClick } from "react-icons/hi";
 import { TbArrowBounce } from "react-icons/tb";
+import { BiRightArrowAlt } from "react-icons/bi";
 import { Separator } from "@/components/ui/separator";
 import CreateFormBtn from "@/components/CreateFormBtn";
 import { Form } from "@prisma/client";
+import { Badge } from "@/components/ui/badge";
+import { formatDistance } from "date-fns";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -126,20 +138,6 @@ function FormCardSkeleton() {
   return <Skeleton className="border-2 border-primary-/20 h-[190px] w-full" />;
 }
 
-function FormCard({ form }: { form: Form }) {
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>
-          <span className="flex items-center gap-2 justify-between">
-            {form.name}
-          </span>
-        </CardTitle>
-      </CardHeader>
-    </Card>
-  );
-}
-
 async function FormCards() {
   const forms = await GetForms();
   return (
@@ -148,5 +146,51 @@ async function FormCards() {
         <FormCard key={form.id} form={form} />
       ))}
     </>
+  );
+}
+
+function FormCard({ form }: { form: Form }) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2 justify-between">
+          <span className="truncate font-bold">{form.name}</span>
+          {form.published && <Badge>Published</Badge>}
+          {!form.published && <Badge variant={"destructive"}>Draft</Badge>}
+        </CardTitle>
+        <CardDescription className="flex items-center justify-between text-muted-foreground text-sm">
+          {formatDistance(form.createdAt, new Date(), {
+            addSuffix: true,
+          })}
+          {!form.published && (
+            <span className="flex items-center gap-2">
+              <LuView className="text-muted-foreground" />
+              <span>{form.visits.toLocaleString()}</span>
+              <FaWpforms className="text-muted-foreground" />
+              <span>{form.submissions.toLocaleString()}</span>
+            </span>
+          )}
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="h-[20px] truncate text-sm text-muted-foreground">
+        {form.description || "No description"}
+      </CardContent>
+      <CardFooter>
+        {form.published && (
+          <Button asChild className="w-full mt-2 text-md gap-4">
+            <Link href={`/forms/${form.id}`}>
+              View Submissions <BiRightArrowAlt />{" "}
+            </Link>
+          </Button>
+        )}
+        {!form.published && (
+          <Button asChild className="w-full mt-2 text-md gap-4">
+            <Link href={`/builder/${form.id}`}>
+              Edit <FaEdit />{" "}
+            </Link>
+          </Button>
+        )}
+      </CardFooter>
+    </Card>
   );
 }
--- components/CreateFormBtn.tsx
+++ components/CreateFormBtn.tsx
@@ -26,8 +26,10 @@ import { Textarea } from "./ui/textarea";
 import { toast } from "./ui/use-toast";
 import { formSchema, formSchemaType } from "@/schemas/form";
 import { CreateForm } from "@/actions/form";
+import { useRouter } from "next/navigation";
 
 function CreateFormBtn() {
+  const router = useRouter();
   const form = useForm<formSchemaType>({
     resolver: zodResolver(formSchema),
   });
@@ -39,7 +41,7 @@ function CreateFormBtn() {
         title: "Success",
         description: "Form created successfully",
       });
-      console.log("form id", formId);
+      router.push(`/builder/${formId}`);
     } catch (error) {
       toast({
         title: "Error",
