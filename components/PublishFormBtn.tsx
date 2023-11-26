94025b2b110cac862048756a91d51d38a755a15f
Eray Keskin <meraykeskin@gmail.com>
2023-11-26 14:50:46 +0300

    add Preview, Publish and Save buttons

---
+++ components/PublishFormBtn.tsx
@@ -0,0 +1,13 @@
+import { MdOutlinePublish } from "react-icons/md";
+import { Button } from "./ui/button";
+
+function PublishFormBtn() {
+  return (
+    <Button className="gap-2 text-white bg-gradient-to-r from-indigo-400">
+      <MdOutlinePublish className="w-4 h-6" />
+      Publish
+    </Button>
+  );
+}
+
+export default PublishFormBtn;
