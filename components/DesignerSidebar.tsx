b098ff36764488d6d6fc617ff6a9bf5ef2fdaafc
Eray Keskin <meraykeskin@gmail.com>
2023-11-26 16:31:08 +0300

    design and design sidebar add page

---
+++ components/DesignerSidebar.tsx
@@ -0,0 +1,9 @@
+function DesignerSidebar() {
+  return (
+    <aside className="w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto h-auto">
+      Elements
+    </aside>
+  );
+}
+
+export default DesignerSidebar;
--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -4,26 +4,33 @@ import { Form } from "@prisma/client";
 import PreviewDialogBtn from "./PreviewDialogBtn";
 import SaveFormBtn from "./SaveFormBtn";
 import PublishFormBtn from "./PublishFormBtn";
+import Designer from "./Designer";
+import { DndContext } from "@dnd-kit/core";
 
 function FormBuilder({ form }: { form: Form }) {
   return (
-    <main className="flex flex-col w-full">
-      <div className="flex justify-between border-b-2 p-4 gap-3 items-center">
-        <h2 className="truncate font-medium">
-          <span className="text-muted-foreground mr-2">Form:</span>
-          {form.name}
-        </h2>
-        <div className="flex items-center gap-2">
-          <PreviewDialogBtn />
-          {!form.published && (
-            <>
-              <SaveFormBtn />
-              <PublishFormBtn />
-            </>
-          )}
+    <DndContext>
+      <main className="flex flex-col w-full">
+        <div className="flex justify-between border-b-2 p-4 gap-3 items-center">
+          <h2 className="truncate font-medium">
+            <span className="text-muted-foreground mr-2">Form:</span>
+            {form.name}
+          </h2>
+          <div className="flex items-center gap-2">
+            <PreviewDialogBtn />
+            {!form.published && (
+              <>
+                <SaveFormBtn />
+                <PublishFormBtn />
+              </>
+            )}
+          </div>
         </div>
-      </div>
-    </main>
+        <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+          <Designer />
+        </div>
+      </main>
+    </DndContext>
   );
 }
 
