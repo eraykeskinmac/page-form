86e807ebb941d6fabea66f08880e8329fe88318a
Eray Keskin <meraykeskin@gmail.com>
2023-11-29 11:05:02 +0300

    Add TextFieldFormElement designer component

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -1,18 +1,48 @@
 "use client";
-
-import { cn } from "@/lib/utils";
 import DesignerSidebar from "./DesignerSidebar";
-import { useDroppable } from "@dnd-kit/core";
-import { useState } from "react";
-import { FormElementInstance } from "./FormElements";
+import { DragEndEvent, useDndMonitor, useDroppable } from "@dnd-kit/core";
+import { cn } from "@/lib/utils";
+import useDesigner from "./hooks/useDesigner";
+import {
+  ElementsType,
+  FormElementInstance,
+  FormElements,
+} from "./FormElements";
+import { idGenerator } from "@/lib/idGenerator";
 
 function Designer() {
+  const { elements, addElement } = useDesigner();
+
   const droppable = useDroppable({
     id: "designer-drop-area",
     data: {
       isDesignerDropArea: true,
     },
   });
+
+  console.log("ELEMENTS", elements);
+
+  useDndMonitor({
+    onDragEnd: (event: DragEndEvent) => {
+      const { active, over } = event;
+      if (!active || !over) return;
+
+      const isDesignerBtnElement = active.data?.current?.isDesignerBtnElement;
+
+      if (isDesignerBtnElement) {
+        const type = active.data?.current?.type;
+        const newElement = FormElements[type as ElementsType].construct(
+          idGenerator()
+        );
+        addElement(0, newElement);
+
+        console.log("NEW ELEMENT", newElement);
+      }
+
+      console.log("DRAG END", event);
+    },
+  });
+
   return (
     <div className="w-full h-full flex">
       <div className="p-4 w-full">
@@ -23,7 +53,7 @@ function Designer() {
             droppable.isOver && "ring-2 ring-primary/20"
           )}
         >
-          {!droppable.isOver && (
+          {!droppable.isOver && elements.length === 0 && (
             <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
               Drop Here
             </p>
@@ -33,9 +63,52 @@ function Designer() {
               <div className="h-[120px] rounded-md bg-primary/20"></div>
             </div>
           )}
+          {elements.length > 0 && (
+            <div className="flex flex-col w-full gap-2 p-4">
+              {elements.map((element) => (
+                <DesignerElementWrapper key={element.id} element={element} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
       <DesignerSidebar />
+    </div>
+  );
+}
+
+function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
+  const DesignerElement = FormElements[element.type].designerComponent;
+  const topHalf = useDroppable({
+    id: element.id + "-top",
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isTopHalfDesignerElement: true,
+    },
+  });
+  const bottomHalf = useDroppable({
+    id: element.id + "-bottom",
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isBottomHalfDesignerElement: true,
+    },
+  });
+
+  return (
+    <div className="relative h-[120px] flex flex-col text-foreground hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset">
+      <div
+        ref={topHalf.setNodeRef}
+        className="absolute w-full h-1/2 rounded-t-md"
+      ></div>
+      <div
+        ref={bottomHalf.setNodeRef}
+        className="absolute bottom-0 w-full h-1/2 rounded-b-md"
+      ></div>
+      <div className="flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none">
+        <DesignerElement elementInstance={element} />
+      </div>
     </div>
   );
 }
