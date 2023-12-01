f22fae168e4702d0a0c6a026691e1442d998d779
Eray Keskin <meraykeskin@gmail.com>
2023-12-02 00:39:11 +0300

    Update "updateElement" function in DesignerContext.tsx and "PropertiesComponent" in TextField.tsx

--- components/context/DesignerContext.tsx
+++ components/context/DesignerContext.tsx
@@ -15,6 +15,7 @@ type DesignerContextType = {
   removeElement: (id: string) => void;
   selectedElement: FormElementInstance | null;
   setSelectedElement: Dispatch<SetStateAction<FormElementInstance | null>>;
+  updateElement: (id: string, element: FormElementInstance) => void;
 };
 
 export const DesignerContext = createContext<DesignerContextType | null>(null);
@@ -40,6 +41,15 @@ export default function DesignerContextProvider({
     setElements((prev) => prev.filter((element) => element.id !== id));
   };
 
+  const updateElement = (id: string, element: FormElementInstance) => {
+    setElements((prev) => {
+      const newElements = [...prev];
+      const index = newElements.findIndex((el) => el.id === id);
+      newElements[index] = element;
+      return newElements;
+    });
+  };
+
   return (
     <DesignerContext.Provider
       value={{
@@ -48,6 +58,7 @@ export default function DesignerContextProvider({
         removeElement,
         selectedElement,
         setSelectedElement,
+        updateElement,
       }}
     >
       {children}
