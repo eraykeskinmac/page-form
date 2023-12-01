a99a2807733ed783f709e8fabc042dbf5370e91a
Eray Keskin <meraykeskin@gmail.com>
2023-12-02 00:38:52 +0300

    Refactor DesignerSidebar and add FormElementsSidebar and PropertiesFormSidebar

--- components/DesignerSidebar.tsx
+++ components/DesignerSidebar.tsx
@@ -1,11 +1,13 @@
-import { FormElements } from './FormElements';
-import SidebarBtnElement from './SidebarBtnElement';
+import useDesigner from './hooks/useDesigner';
+import FormElementsSidebar from './FormElementsSidebar';
+import PropertiesFormSidebar from './PropertiesFormSidebar';
 
 function DesignerSidebar() {
+  const { selectedElement } = useDesigner();
   return (
     <aside className="w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto h-auto">
-      Elements
-      <SidebarBtnElement formElement={FormElements.TextField} />
+      {!selectedElement && <FormElementsSidebar />}
+      {selectedElement && <PropertiesFormSidebar />}
     </aside>
   );
 }
