efd8578791de4c2a3ef0a58ad8bc7fadb7e90a8d
Eray Keskin <meraykeskin@gmail.com>
2023-12-06 01:26:02 +0300

    Update TextField styles to use "gap" instead of "gpa" for spacing

--- components/fields/TextField.tsx
+++ components/fields/TextField.tsx
@@ -224,7 +224,7 @@ function FormComponent({
   const element = elementInstance as CustomInstance;
   const { label, required, placeHolder, helperText } = element.extraAttributes;
   return (
-    <div className="flex flex-col gpa-2 w-full">
+    <div className="flex flex-col gap-2 w-full">
       <Label>
         {label}
         {required && '*'}
