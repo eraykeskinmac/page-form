84ced013548837ffb3724298c42ce092ffe21bff
Eray Keskin <meraykeskin@gmail.com>
2023-12-06 18:54:24 +0300

    Publish form functionality added

--- components/PublishFormBtn.tsx
+++ components/PublishFormBtn.tsx
@@ -1,12 +1,75 @@
 import { MdOutlinePublish } from 'react-icons/md';
 import { Button } from './ui/button';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from './ui/alert-dialog';
+import { FaIcons } from 'react-icons/fa';
+import { useTransition } from 'react';
+import { toast } from './ui/use-toast';
+import { PublishForm } from '@/actions/form';
+import { useRouter } from 'next/navigation';
 
-function PublishFormBtn() {
+function PublishFormBtn({ id }: { id: number }) {
+  const [loading, startTransition] = useTransition();
+  const router = useRouter();
+
+  async function publishForm() {
+    try {
+      await PublishForm(id);
+      toast({
+        title: 'Success',
+        description: 'Your form is now available to the public',
+      });
+      router.refresh();
+    } catch (error) {
+      toast({
+        title: 'Error',
+        description: 'Something went wrong',
+      });
+    }
+  }
   return (
-    <Button className="gap-2 text-white bg-gradient-to-r from-indigo-400">
-      <MdOutlinePublish className="w-4 h-6" />
-      Publish
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="gap-2 text-white bg-gradient-to-r from-indigo-400">
+          <MdOutlinePublish className="w-4 h-6" />
+          Publish
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. After publishing you will not be aple
+            to edit this form. <br /> <br />
+            <span className="font-medium">
+              By publishing this form you will make it available to the public
+              and you will be able to collect submissions.
+            </span>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={loading}
+            onClick={(e) => {
+              e.preventDefault();
+              startTransition(publishForm);
+            }}
+          >
+            Proceed {loading && <FaIcons className="animate-spin" />}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
 
