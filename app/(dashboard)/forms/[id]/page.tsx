014e821bac0d03eeec9d8bc730d7f8d41b51ca2d
Eray Keskin <meraykeskin@gmail.com>
2023-12-10 01:35:37 +0300

    Refactor form detail page with new UI components and layout

--- app/(dashboard)/forms/[id]/page.tsx
+++ app/(dashboard)/forms/[id]/page.tsx
@@ -1,6 +1,13 @@
 import { GetFormById } from '@/actions/form';
+import FormLinkShare from '@/components/FormLinkShare';
+import VisitBtn from '@/components/VisitBtn';
+import { StatsCard } from '../../page';
+import { LuView } from 'react-icons/lu';
+import { FaWpforms } from 'react-icons/fa';
+import { HiCursorClick } from 'react-icons/hi';
+import { TbArrowBounce } from 'react-icons/tb';
 
-async function BuilderPage({
+async function FormDetailPage({
   params,
 }: {
   params: {
@@ -13,7 +20,74 @@ async function BuilderPage({
   if (!form) {
     throw new Error('form not found');
   }
-  return <>Details</>;
+  const { visits, submissions } = form;
+  let submissionRate = 0;
+
+  if (visits > 0) {
+    submissionRate = (submissions / visits) * 100;
+  }
+  const bounceRate = 100 - submissionRate;
+  return (
+    <>
+      <div className="py-10  border-b border-muted">
+        <div className="flex justify-between container">
+          <h1 className="text-4xl font-bold truncate">{form.name}</h1>
+          <VisitBtn shareUrl={form.shareURL} />
+        </div>
+      </div>
+      <div className="py-4 border-b border-muted">
+        <div className="container flex gap-2 items-center justify-between">
+          <FormLinkShare shareUrl={form.shareURL} />
+        </div>
+      </div>
+      <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 container">
+        <StatsCard
+          title="Total visits"
+          icon={<LuView className="text-blue-600" />}
+          helperText="All time form visits"
+          value={visits.toLocaleString() || ''}
+          loading={false}
+          className="shadow-md shadow-blue-600"
+        />
+        <StatsCard
+          title="Total submission"
+          icon={<FaWpforms className="text-yellow-600" />}
+          helperText="All time form submission"
+          value={submissions.toLocaleString() || ''}
+          loading={false}
+          className="shadow-md shadow-yellow-600"
+        />
+        <StatsCard
+          title="Submission rate"
+          icon={<HiCursorClick className="text-green-600" />}
+          helperText="Visits that result in form submission"
+          value={submissionRate.toLocaleString() + '%' || ''}
+          loading={false}
+          className="shadow-md shadow-green-600"
+        />
+        <StatsCard
+          title="Bounce Rate"
+          icon={<TbArrowBounce className="text-red-600" />}
+          helperText="Visits that leaves without interacting"
+          value={bounceRate.toLocaleString() + '%' || ''}
+          loading={false}
+          className="shadow-md shadow-red-600"
+        />
+      </div>
+
+      <div className="container pt-10">
+        <SubmissionsTable id={form.id} />
+      </div>
+    </>
+  );
 }
 
-export default BuilderPage;
+export default FormDetailPage;
+
+function SubmissionsTable({ id }: { id: number }) {
+  return (
+    <>
+      <h1 className="text-2xl font-bold my-4">Submissions</h1>
+    </>
+  );
+}
