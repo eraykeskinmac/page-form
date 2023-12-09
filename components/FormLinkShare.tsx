b09a4a73468f2fa2dd194f6e6fddf9ebb020fdb8
Eray Keskin <meraykeskin@gmail.com>
2023-12-10 01:35:20 +0300

    Add FormLinkShare component

---
+++ components/FormLinkShare.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { ImShare } from 'react-icons/im';
+import { toast } from './ui/use-toast';
+
+function FormLinkShare({ shareUrl }: { shareUrl: string }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null; // avoiding window not defined error
+  }
+  const shareLink = `${window.location.origin}/submit/${shareUrl}`;
+  return (
+    <div className="flex flex-grow gap-4 items-center">
+      <Input value={shareLink} readOnly />
+      <Button
+        className="w-[250px]"
+        onClick={() => {
+          navigator.clipboard.writeText(shareLink);
+          toast({
+            title: 'Copied!',
+            description: 'Linnk copied to clipboard',
+          });
+        }}
+      >
+        <ImShare className="mr-2 h-4 w-4" />
+        Share Link
+      </Button>
+    </div>
+  );
+}
+
+export default FormLinkShare;
